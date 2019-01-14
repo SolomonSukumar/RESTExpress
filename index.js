8c337fd597e02c5bab1e76e83458e0e604a0b252
SolomonSukumar <solo007mon@gmail.com>
2019-01-14 17:15:40 -0500

    .

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+const Joi = require('joi');
+const logger = require('./middleware/logger');
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
+
 const app = express();
 
-app.get('/', (req, res) => {
+//view engine 
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-    res.send('Hello World');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/courses', courses);
+app.use('/', home);
 
-} );
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server:' + config.get('mail.host'));
 
-app.get('/courses', (req, res) => {
+if(app.get('env') == 'development'){
+    app.use(morgan('tiny'));
+    startupDebugger('Morgan is enabled.....');
+}
 
-    res.send([1, 2, 3, 4]);
-});
+//db work
+dbDebugger('Connected to database.....');
+
+app.use(logger.log);
+app.use(logger.auth);
 
 //PORT to assign port dynamically
 const port = process.env.PORT || 3000
---
+++ middleware/logger.js
@@ -0,0 +1,15 @@
+
+function log (req, res, next){
+
+    console.log('Logging......');
+    next();
+}
+
+function auth (req, res, next){
+    console.log('Authenticating....');
+    next();
+}
+
+
+module.exports.log = log;
+module.exports.auth = auth;
